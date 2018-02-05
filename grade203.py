956ba8f86449bab547f4617e3d5da9ed10589338
Eric Zair <zaireric@gmail.com>
2018-02-05 12:12:27 -0500

    Update grade203.py

--- grade203.py
+++ grade203.py
@@ -14,7 +14,8 @@ Description:	This file grades 203 Hw Assignments.
 import os
 import shutil
 from os import system
-from pyunpack import Archive
+from pyunpack import Archive #run the below command in order to install this dependencies
+			     #sudo apt-get install pyunpack && sudo apt-get
 
 
 '''
